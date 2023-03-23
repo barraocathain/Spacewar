6ad92bf07a709456a96d525ebb16ef6c458d2431
Barry Kane <barra@ocathain.ie>
2023-03-23 13:46:59 +0000

    Make ships stop after teleporting through the wormhole.

--- spacewarPlayer.h
+++ spacewarPlayer.h
@@ -44,6 +44,8 @@ static inline void calculateGravity(xyVector * starPosition, ship * shipUnderGra
 		{
 			shipUnderGravity->position.xComponent = random() % 4000;
 			shipUnderGravity->position.yComponent = random() % 4000;
+			shipUnderGravity->velocity.xComponent = 0;
+			shipUnderGravity->velocity.yComponent = 0;
 		}
 	}
 	else
